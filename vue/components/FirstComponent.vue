b5d2ede0033ae939de935428c9d4d585d5e3b115
Fregene Thomas <fregzythomas@gmail.com>
2021-04-15 17:46:51 +0100

    adding configuration for laravel-mix

---
+++ vue/components/FirstComponent.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+The year is {{year}}
+</div>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            year: new Date().getFullYear()
+        }
+    }
+}
+</script>
